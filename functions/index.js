7b5105140d5ed9510e0f4f5833738e2ef36bf7fa
maddes <sdeonline@gmail.com>
2020-10-26 00:44:30 -0300

    logs

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ admin.initializeApp()
 // Cloud Firestore under the path /authors/:documentId/original
 exports.addAuthor = functions.https.onRequest(async (req, res) => {
   const author = await Author.add(req.body.name)
+  console.log('author', author)
   // Send back a message that we've succesfully written the message
   res.json({
     result: 'success',
